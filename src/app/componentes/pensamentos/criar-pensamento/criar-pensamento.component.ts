ff942c529b364af148453d309d77d7cba3275dd6
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-11-16 15:32:12 -0300

    fluxo de dados

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public criarPensamento(): void{
+
+  }
+
+  public cancelar(): void{
+
   }
 
 }
