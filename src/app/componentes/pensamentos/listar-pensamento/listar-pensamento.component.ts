387194bcd580c82ec9e84cb47ba7ebc2f00e7d9b
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-11-18 09:18:20 -0300

    backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,3 +1,4 @@
+import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  public listaPensamentos = [
-    {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Angular',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Pensamento entre leitores',
-      autoria: 'Anônimo',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-      autoria: 'Dev',
-      modelo: 'modelo3'
-    }
-  ]
+  public listaPensamentos: Pensamento[] = []
 
   constructor() { }
 
